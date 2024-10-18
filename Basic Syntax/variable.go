f708ce2c115d35fccbcd68291c7e131e0cc72976
Febriyan Biopsa <99369881+FebriyanBiopsaMinanda@users.noreply.github.com>
2024-10-18 13:24:19 +0700

    Add files via upload

---
+++ Basic Syntax/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var x = 12
+
+func main() {
+	var myName string = "Febriyan Biopsa Minanda"
+	var userName = "Iyan"
+	var age int = 20
+
+	result := x * 2
+
+	fmt.Println(myName)
+	fmt.Println(userName)
+	fmt.Println(age)
+
+
+	fmt.Println("Hasil Perkalian :", result)
+}
